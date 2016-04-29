04899dbac017f20bf214863838b163840e84d1eb
Joe <joe@chetcodigital.com>
2016-04-29 12:53:24 -0800

    initial push

--- web/app.py
+++ web/app.py
@@ -50,13 +50,14 @@ from flask import (
 
 #import db
 
-
+"""
 from psycopg2.pool import ThreadedConnectionPool
 db_pool = ThreadedConnectionPool(
   1, # min connections,
   int(os.environ.get('MAX_DB_CONNECTIONS',3)),
   **connection_from(os.environ['DATABASE_URL'])
 )
+"""
 
 @app.route('/')
 def index():
