b9920fc52293979cbdef4e57f4c23861468554dd
Joe <joe@chetcodigital.com>
2016-07-21 12:25:56 -0800

    jsonp

--- web/app.py
+++ web/app.py
@@ -1584,13 +1584,13 @@ def freeboard_engine():
         #return jsonify(date_time=mydatetime, update=False, status='error', rpm=value1, eng_temp=value2, oil_pressure=value3, alternator=value4, boost=value5, fuel_rate=value6, fuel_level=value7, eng_hours=value8)
         callback = request.args.get('callback')
         myjsondate = mydatetime.strftime("%B %d, %Y %H:%M:%S")
-        return '{0}({1})'.format(callback, {'date_time':myjsondate, 'update':'False','status'='error', 'rpm':value1, 'eng_temp':value2, 'oil_pressure':value3, 'alternator':value4, 'boost':value5, 'fuel_rate':value6, 'fuel_level':value7, 'eng_hours':value8})
+        return '{0}({1})'.format(callback, {'date_time':myjsondate, 'update':'False','status':'error', 'rpm':value1, 'eng_temp':value2, 'oil_pressure':value3, 'alternator':value4, 'boost':value5, 'fuel_rate':value6, 'fuel_level':value7, 'eng_hours':value8})
 
     mydatetime = datetime.datetime.now()
     #return jsonify(date_time=mydatetime, update=False, status='error', rpm=value1, eng_temp=value2, oil_pressure=value3, alternator=value4, boost=value5, fuel_rate=value6, fuel_level=value7, eng_hours=value8)
     callback = request.args.get('callback')
     myjsondate = mydatetime.strftime("%B %d, %Y %H:%M:%S") 
-    return '{0}({1})'.format(callback, {'date_time':myjsondate, 'update':'False','status'='error', 'rpm':value1, 'eng_temp':value2, 'oil_pressure':value3, 'alternator':value4, 'boost':value5, 'fuel_rate':value6, 'fuel_level':value7, 'eng_hours':value8})
+    return '{0}({1})'.format(callback, {'date_time':myjsondate, 'update':'False','status':'error', 'rpm':value1, 'eng_temp':value2, 'oil_pressure':value3, 'alternator':value4, 'boost':value5, 'fuel_rate':value6, 'fuel_level':value7, 'eng_hours':value8})
 
 @app.route('/freeboard_status')
 def freeboard_status():
