cc269188a9eafa6bc1f77365ec5739ccdc12deda
Joe <joe@chetcodigital.com>
2016-08-06 13:18:03 -0800

    influx test

--- web/app.py
+++ web/app.py
@@ -573,16 +573,16 @@ def freeboard_createInfluxDB():
     ]
   """
   json_body=[]
-  timestamp = "2016-08-06 17:45:24"
+  timestamp = "2016-08-06 17:55:24"
 
   Key1="deviceid:001EC010AD69.sensor:environmental_data.source:0.instance:0.type:Outside_Temperature.parameter:temperature.HelmSmart"
   Key2="deviceid:001EC010AD69.sensor:environmental_data.source:0.instance:0.type:Outside_Temperature.parameter:humidity.HelmSmart"
   Key3="deviceid:001EC010AD69.sensor:environmental_data.source:0.instance:0.type:Outside_Temperature.parameter:atmospheric_pressure.HelmSmart"
 
   
-  json_body.append(convert_influxdbcloud_json(Key1, timestamp, 110.0))
-  json_body.append(convert_influxdbcloud_json(Key2, timestamp, 50.0))
-  json_body.append(convert_influxdbcloud_json(Key3, timestamp, 2234.0))
+  json_body.append(convert_influxdbcloud_json(Key1, timestamp, 120.0))
+  json_body.append(convert_influxdbcloud_json(Key2, timestamp, 60.0))
+  json_body.append(convert_influxdbcloud_json(Key3, timestamp, 24234.0))
 
   log.info("freeboard Create InfluxDB json_body:%s", json_body)
   log.info("freeboard Create InfluxDB %s", database)
@@ -610,7 +610,8 @@ def freeboard_createInfluxDB():
 
     log.info("freeboard Get InfluxDB points %s", database)
 
-    query = 'select * from HelmSmart;'
+    #query = 'select * from HelmSmart;'
+    query = 'select * from HelmSmart WHERE parameter=temperature'
     result = db.query(query)
 
     log.info("freeboard Get InfluxDB results %s", result)
