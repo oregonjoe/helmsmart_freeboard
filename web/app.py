530178b53d3e4cb8d205d055aed5429b11553c00
Joe <joe@chetcodigital.com>
2016-08-08 12:43:37 -0800

    influx cloud test

--- web/app.py
+++ web/app.py
@@ -696,7 +696,7 @@ def freeboard_createInfluxDB():
 
     log.info("freeboard Get InfluxDB results %s", result)
 
-    keys = result.keys
+    keys = result.get('series',[])
     log.info("freeboard Get InfluxDB series keys%s", keys)
 
     for key in keys:
