85a4951c79f6cde45595931c4b88ce2671a20032
Joe <joe@chetcodigital.com>
2016-08-09 10:39:08 -0800

    influx test

--- web/app.py
+++ web/app.py
@@ -751,8 +751,8 @@ def freeboard_createInfluxDB():
     for series in keys:
       log.info("freeboard Get InfluxDB series key %s", series)
       #log.info("freeboard Get InfluxDB series deviceID %s ", series['deviceid'])
-       for tags in series:
-         log.info("freeboard Get InfluxDB tags %s ", tags)
+      for tags in series:
+        log.info("freeboard Get InfluxDB tags %s ", tags)
  
     return jsonify( message='freeboard_createInfluxDB', status='error')
 
