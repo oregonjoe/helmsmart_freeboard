0fe5cc2717bf96344a8fc6dfa52d9ca86d87c685
Joe <joe@chetcodigital.com>
2016-11-27 19:26:15 -0800

    freeboard ac params

--- web/static/plugins/helmsmart.widgets.js
+++ web/static/plugins/helmsmart.widgets.js
@@ -1470,6 +1470,104 @@
         }
     });
 
-   
+    var pictureWidget = function(settings)
+    {
+        var self = this;
+        var widgetElement;
+        var timer;
+        var imageURL;
+
+        function stopTimer()
+        {
+            if(timer)
+            {
+                clearInterval(timer);
+                timer = null;
+            }
+        }
+
+        function updateImage()
+        {
+            if(widgetElement && imageURL)
+            {
+                var cacheBreakerURL = imageURL + (imageURL.indexOf("?") == -1 ? "?" : "&") + Date.now();
+
+                $(widgetElement).css({
+                    "background-image" :  "url(" + cacheBreakerURL + ")"
+                });
+            }
+        }
+
+        this.render = function(element)
+        {
+            $(element).css({
+                width : "100%",
+                height: "100%",
+                "background-size" : "cover",
+                "background-position" : "center"
+            });
+
+            widgetElement = element;
+        }
+
+        this.onSettingsChanged = function(newSettings)
+        {
+            stopTimer();
+
+            if(newSettings.refresh && newSettings.refresh > 0)
+            {
+                timer = setInterval(updateImage, Number(newSettings.refresh) * 1000);
+            }
+        }
+
+        this.onCalculatedValueChanged = function(settingName, newValue)
+        {
+            if(settingName == "src")
+            {
+				index = newValue[0].value;
+				
+				index = Math.floor(index/10);
+			
+                imageURL = "https://helmsmart-freeboard.herokuapp.com/static/plugins/images/Large_Compass_Rotating_Black_" + index +".jpg";
+            }
+
+            updateImage();
+        }
+
+        this.onDispose = function()
+        {
+            stopTimer();
+        }
+
+        this.getHeight = function()
+        {
+            return 4;
+        }
+
+        this.onSettingsChanged(settings);
+    };
+
+    freeboard.loadWidgetPlugin({
+        type_name: "hspicture",
+        display_name: "HelmSmart Picture",
+        fill_size: true,
+        settings: [
+            {
+                name: "src",
+                display_name: "Image URL",
+                type: "calculated"
+            },
+            {
+                "type": "number",
+                "display_name": "Refresh every",
+                "name": "refresh",
+                "suffix": "seconds",
+                "description":"Leave blank if the image doesn't need to be refreshed"
+            }
+        ],
+        newInstance: function (settings, newInstanceCallback) {
+            newInstanceCallback(new pictureWidget(settings));
+        }
+    });
 
 }());
