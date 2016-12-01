6411e32ab8aacb7dfc478d78b3f5c9e75e76c76d
Joe <joe@chetcodigital.com>
2016-11-30 17:08:15 -0800

    freeboard ac params

--- web/static/plugins/helmsmart.widgets.js
+++ web/static/plugins/helmsmart.widgets.js
@@ -2394,9 +2394,9 @@
 				
 			///var url = "http://www.helmsmart.net/sendswitchapi?deviceapikey=2c76ae5a6f20125071a988b23cd4a6c8&switch=$PCDIN,01F20E,00000000,00,01010000FFFFFFFF*24"	
 			
-			var url = "https://pushsmartdata.herokuapp.com/sendswitchapi?deviceapikey=2c76ae5a6f20125071a988b23cd4a6c8&switch=$PCDIN,01F20E,00000000,00,01010000FFFFFFFF*24"
+			//var url = "https://pushsmartdata.herokuapp.com/sendswitchapi?deviceapikey=2c76ae5a6f20125071a988b23cd4a6c8&switch=$PCDIN,01F20E,00000000,00,01010000FFFFFFFF*24"
 
-
+			var url = "https://thingproxy.freeboard.io/fetch/https://pushsmartdata.herokuapp.com/sendswitchapi?deviceapikey=2c76ae5a6f20125071a988b23cd4a6c8&switch=$PCDIN,01F20E,00000000,00,01010000FFFFFFFF*24"
 				
 			request = new XMLHttpRequest();
             if (!request) {
