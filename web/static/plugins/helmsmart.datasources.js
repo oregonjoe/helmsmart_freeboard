c287f59b81280cb2171b672b89e1158393dfe76c
Joe <joe@chetcodigital.com>
2017-10-13 10:09:16 -0700

    dimmer datasource update

--- web/static/plugins/helmsmart.datasources.js
+++ web/static/plugins/helmsmart.datasources.js
@@ -429,7 +429,67 @@
 					{
 						name: "9",
 						value: "9"
-					}
+					},
+					{
+						name: "10",
+						value: "10"
+					},
+					{
+						name: "11",
+						value: "11"
+					},
+					{
+						name: "12",
+						value: "12"
+					},					
+					{
+						name: "13",
+						value: "13"
+					},					
+					{
+						name: "14",
+						value: "14"
+					},					
+					{
+						name: "15",
+						value: "15"
+					},					
+					{
+						name: "16",
+						value: "16"
+					},					
+					{
+						name: "17",
+						value: "17"
+					},					
+					{
+						name: "18",
+						value: "18"
+					},					
+					{
+						name: "19",
+						value: "19"
+					},
+					{
+						name: "20",
+						value: "20"
+					},
+					{
+						name: "21",
+						value: "21"
+					},
+					{
+						name: "22",
+						value: "22"
+					},					
+					{
+						name: "23",
+						value: "23"
+					},
+					{
+						name: "24",
+						value: "24"
+					}					
 				]
 			},
 			{
