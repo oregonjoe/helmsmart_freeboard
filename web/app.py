eb3ec57128edeeffd8627f274c902c82f5a852d1
Joe <joe@chetcodigital.com>
2018-07-19 11:28:48 -0700

    add extended egt temp

--- web/app.py
+++ web/app.py
@@ -8128,7 +8128,7 @@ def freeboard_indicator_runtime():
 
       callback = request.args.get('callback')
       myjsondate= mydatetimetz.strftime("%B %d, %Y %H:%M:%S")  
-      return '{0}({1})'.format(callback, {'date_time':myjsondate, 'status':'success','update':'True','vaule':list(reversed(vaule)), 'runtime':list(reversed(runtime)), 'cycles':list(reversed(cycles)))})     
+      return '{0}({1})'.format(callback, {'date_time':myjsondate, 'status':'success','update':'True','vaule':list(reversed(vaule)), 'runtime':list(reversed(runtime)), 'cycles':list(reversed(cycles ))})     
 
 
 
