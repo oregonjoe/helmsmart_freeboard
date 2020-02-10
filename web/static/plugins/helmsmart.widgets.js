87c3e5143c6154875c69c43791c6c629245e7d5d
Joe <joe@chetcodigital.com>
2020-02-10 13:47:07 -0800

    adding location_wind

--- web/static/plugins/helmsmart.widgets.js
+++ web/static/plugins/helmsmart.widgets.js
@@ -3524,7 +3524,7 @@
 						//markers[i] = new google.maps.Marker({map: map, icon : {labelOrigin: { x: 12, y: -10}}, title: title, label : {text : "label " + i, color: '#222222', fontSize: '12px'}});
 						//markers[i] = new google.maps.Marker({map: map,  title: title, label : {text : "label " + i, color: '#222222', fontSize: '12px'}});
 						
-						markers[i] = new google.maps.Marker({map: map, icon: { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, scale: 10, strokeColor : #222222 , rotation : 30 }, title: title, label : {text : "label " + i, color: '#222222', fontSize: '12px'}});
+						markers[i] = new google.maps.Marker({map: map, icon: { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, scale: 10, strokeColor : '#222288' , rotation : 30 }, title: title, label : {text : "label " + i, color: '#222222', fontSize: '12px'}});
 						
 						//markers[i] = new google.maps.Marker({map: map, icon : {labelOrigin: { x: 12, y: -10}}, title: title});
 			
