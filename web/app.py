aad2dcac7ab29b6e1416a793ed6900caba0da88f
Joe <joe@chetcodigital.com>
2020-05-29 10:18:52 -0700

    adding in metar

--- web/app.py
+++ web/app.py
@@ -5978,7 +5978,7 @@ def helmsmart_environmental_baroncsv():
       
       csvstr = "ALBN,05/27/2020,12:20,76.0,68.0,64.0,76,76,S,7,12,,0.1,79,64"
 
-      csvstr = ('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,0.0,79,64' %  (stationid, mycsvdate, mycsvtime, temperature, humidity,dewpoint, windchill, heatindex, wind_dir, wind_speed, wind_gust, atmospheric_pressure,  ))
+      csvstr = ('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s' %  (stationid, mycsvdate, mycsvtime, temperature, humidity,dewpoint, windchill, heatindex, wind_dir, wind_speed, wind_gust, atmospheric_pressure,rain, temphigh, templow  ))
 
         
       csvout =  "ID,DATE,TIME,TEMP,RH,DEWPT,WINDCHILL,HEATINDEX,WDIR,WSPEED,WGUST,PRESSURE,PRECIP,HIGH,LOW" + '\r\n' + csvstr + '\r\n'
