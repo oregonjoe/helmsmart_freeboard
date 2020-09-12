b26ec7159472a1a9f17fd85e02b1c182640f6bb6
Joe <joe@chetcodigital.com>
2020-09-12 12:25:16 -0700

    test simple json

--- web/app.py
+++ web/app.py
@@ -1813,15 +1813,15 @@ def freeboad_simplejson_test():
     {
       "target":"upper_75", #The field being queried for
       "datapoints":[
-        [622,1450754160000],  #Metric value as a float , unixtimestamp in milliseconds
-        [365,1450754220000]
+        [622,1599938657000],  #Metric value as a float , unixtimestamp in milliseconds
+        [365,1599938658000]
       ]
     },
     {
       "target":"upper_90",
       "datapoints":[
-        [861,1450754160000],
-        [767,1450754220000]
+        [861,1599938657000],
+        [767,1599938658000]
       ]
     }
   ])
