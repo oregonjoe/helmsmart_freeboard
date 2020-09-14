3e79076a4c55578f22c6bab6d8d874f2015dba84
Joe <joe@chetcodigital.com>
2020-09-13 19:52:36 -0700

    test simple json

--- web/app.py
+++ web/app.py
@@ -2276,7 +2276,7 @@ def qrafana_acstatus():
   #log.info("freeboad_simplejson_test: %s", request.headers)
   log.info("freeboad_simplejson_testrequest.authorization: %s", request.authorization)
   
-  return jsonify("status":"success")
+  return jsonify({"status":"success"})
    
 
 
