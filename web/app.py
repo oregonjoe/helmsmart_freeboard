707922ae87a8329a3ef6852a18d2595d62e55249
oregonjoe <joe@chetcodigital.com>
2022-12-30 15:30:30 -0800

    wug rain

--- web/app.py
+++ web/app.py
@@ -9118,7 +9118,8 @@ def freeboard_rain_wung():
       log.info('freeboard:  WUNG RAIN points%s:', points)
 
       log.info('freeboard:  WUNG RAIN point%s:', points[0]['accumulation'])
-
+      log.info('freeboard:  WUNG RAIN point%s:', points[len(points)-1]['accumulation'])
+      
       for point in points:
          if point['accumulation'] is not None:
            rain_day.append(point['accumulation'])
