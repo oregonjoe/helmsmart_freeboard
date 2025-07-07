d9d9b3ae91fc11c22e52a81c5135dc8b0b5151b2
Joe Burke <joe@chetcodigital.com>
2025-07-07 09:13:26 -0700

    test pull

--- web/app.py
+++ web/app.py
@@ -50,8 +50,8 @@ import logging
 # Debug Output defines
 # Comment to enable/disable
 # ********************************************************************
-#debug_all = False
-debug_all = True
+debug_all = False
+#debug_all = True
 
    
 
